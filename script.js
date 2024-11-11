43f1e804846264036136138da3be3219061d13db
Akib-Hasaan <akibhasan582@gmail.com>
2024-11-11 21:54:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//(1) Arithmetic operations via javascript
+let w = "taka"
+let x = "Amar lagbe holo"
+let y = 500
+let z = 100
+let a = x+" "+"="+" "+y*z+" "+w
+console.log(a);
+
+// (2) Ei khetre number thakleo data type ta string hishabe count kora hoise
+console.log(typeof a);
